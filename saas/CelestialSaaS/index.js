ff9cfd67393bd1059416fa137d14a9bff72c90eb
paul <paul.sajo.official@gmail.com>
2024-07-10 22:28:45 +0530

    working on saas template

---
+++ saas/CelestialSaaS/index.js
@@ -0,0 +1,52 @@
+// initialization
+
+const RESPONSIVE_WIDTH = 1024
+
+let headerWhiteBg = false
+let isHeaderCollapsed = window.innerWidth < RESPONSIVE_WIDTH
+const collapseBtn = document.getElementById("collapse-btn")
+const collapseHeaderItems = document.getElementById("collapsed-header-items")
+
+
+
+function onHeaderClickOutside(e) {
+
+    if (!collapseHeaderItems.contains(e.target)) {
+        toggleHeader()
+    }
+
+}
+
+
+function toggleHeader() {
+    if (isHeaderCollapsed) {
+        // collapseHeaderItems.classList.remove("max-md:tw-opacity-0")
+        collapseHeaderItems.classList.add("opacity-100",)
+        collapseHeaderItems.style.width = "60vw"
+        collapseBtn.classList.remove("bi-list")
+        collapseBtn.classList.add("bi-x", "max-lg:tw-fixed")
+        isHeaderCollapsed = false
+
+        setTimeout(() => window.addEventListener("click", onHeaderClickOutside), 1)
+
+    } else {
+        collapseHeaderItems.classList.remove("opacity-100")
+        collapseHeaderItems.style.width = "0vw"
+        collapseBtn.classList.remove("bi-x", "max-lg:tw-fixed")
+        collapseBtn.classList.add("bi-list")
+        isHeaderCollapsed = true
+        window.removeEventListener("click", onHeaderClickOutside)
+
+    }
+}
+
+function responsive() {
+    if (window.innerWidth > RESPONSIVE_WIDTH) {
+        collapseHeaderItems.style.width = ""
+
+    } else {
+        isHeaderCollapsed = true
+    }
+}
+
+window.addEventListener("resize", responsive)
